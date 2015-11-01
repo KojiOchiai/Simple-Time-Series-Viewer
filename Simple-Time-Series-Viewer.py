56caa36320a1f4237b86302693d47a31968a8b7e
koji ochiai <none>
2015-11-02 00:57:17 +0900

    add menu bar and file select dialog.

--- Simple-Time-Series-Viewer.py
+++ Simple-Time-Series-Viewer.py
@@ -7,14 +7,48 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 matplotlib.style.use('ggplot')
+import os.path
 
 from PyQt4 import QtGui
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
-class Window(QtGui.QWidget):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+class Window(QtGui.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        exitAction = QtGui.QAction('&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtGui.qApp.quit)
+
+        openAction = QtGui.QAction('&Open', self)
+        openAction.setShortcut('Ctrl+O')
+        openAction.setStatusTip('Open CSV File')
+        openAction.triggered.connect(self.open_file)
+        
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        fileMenu.addAction(openAction)
+
+        self.graph = Graph(self) 
+        self.setCentralWidget(self.graph)
+        
+        self.setWindowTitle('Simple Time Series Viewer')
+
+    def open_file(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
+                                                     os.path.expanduser('~'))
+        # import data
+        tsd = pd.DataFrame.from_csv(filename)
+        self.graph.plot(tsd)
+
+class Graph(QtGui.QWidget):
+    def __init__(self, parent):
+        super(Graph, self).__init__(parent)
 
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
@@ -22,21 +56,13 @@ class Window(QtGui.QWidget):
         # set navigation bar
         self.toolbar = NavigationToolbar(self.canvas, self)
 
-        # add button
-        self.button = QtGui.QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
-
         # set the layout
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
         self.setLayout(layout)
 
-    def plot(self):
-        # import data
-        tsd = pd.DataFrame.from_csv('sensor.csv')
-        
+    def plot(self, tsd):
         # data for plot
         time = [mdates.date2num(idx.to_datetime()) for idx in tsd.index]
         y1 = tsd['brightness'].as_matrix()
