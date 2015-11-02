3d55ede00375c7c0cf4345c1d403b42df66c86cf
koji ochiai <none>
2015-11-02 16:54:59 +0900

    1) Show column name. 2) Show legend. 3) Pan buttun is default on.

--- Simple-Time-Series-Viewer.py
+++ Simple-Time-Series-Viewer.py
@@ -48,7 +48,7 @@ class Window(QtGui.QMainWindow):
         data = pd.DataFrame.from_csv(filename)
         self.statusBar().showMessage('')
         self.statusBar().showMessage('ploting...')
-        self.graph.data = data
+        self.graph.set_data(data)
         self.graph.column_plot[0] = [data.columns[0], data.columns[1]]
         self.graph.plot()
         self.statusBar().showMessage('')
@@ -59,10 +59,12 @@ class Graph(QtGui.QWidget):
 
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
+        self._data = None
         self.column_plot = [[]]
 
         # set navigation bar
         self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar.pan()
 
         # column list
         self.column_list = QtGui.QListWidget()
@@ -80,8 +82,17 @@ class Graph(QtGui.QWidget):
         hlayout.addWidget(self.column_list)
         self.setLayout(hlayout)
 
+    def set_data(self, data):
+        self._data = data
+        for label in self._data.columns:
+            item = QtGui.QListWidgetItem(label)
+            self.column_list.addItem(item)
+
+    def get_Data(self):
+        return self._data
+
     def plot(self):
-        if self.data.empty:
+        if self._data.empty:
             return
 
         # create an axis
@@ -90,18 +101,19 @@ class Graph(QtGui.QWidget):
         # discards the old graph
         ax.hold(False)
 
-        for columname in self.column_plot[0]:
+        for column_name in self.column_plot[0]:
             # data for plot
             time = [mdates.date2num(idx.to_datetime())
-                    for idx in self.data.index]
-            y = self.data[columname].as_matrix()
+                    for idx in self._data.index]
+            y = self._data[column_name].as_matrix()
         
             # plot data
             ax.hold(True)
-            ax.plot_date(time, y, '-')
+            ax.plot_date(time, y, '-', label=column_name)
             self.figure.autofmt_xdate()
         
         # refresh canvas
+        plt.legend()
         self.canvas.draw()
 
 if __name__ == '__main__':
